79ec7cbaad02bdf51ea86deed4ea320c3d988904
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-20 22:46:59 +0300

    Массив трехзначных положительных чисел и количество четных/нечетных чисел

---
+++ Work_03/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задать массив, заполнить случайными положительными трёхзначными числами.
+Показать количество нечетных\четных чисел */
+
+int[] array = new int[10];
+int evenNumbers = 0;
+int notEvenNumbers = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 120);
+    Console.Write(array[i] +" ");
+}
+Console.WriteLine();
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] % 2 == 0)
+    {
+        evenNumbers++;
+    }
+
+    else
+    {
+        notEvenNumbers++;
+    }
+}
+Console.Write($"Количество четных чисел массива: {evenNumbers}");
+Console.WriteLine();
+Console.Write($"Количество нечетные чисел массива: {notEvenNumbers}");
